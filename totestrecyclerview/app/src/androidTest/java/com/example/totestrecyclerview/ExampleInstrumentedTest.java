532a3b9a5b189ff5f48c80ac14f8dd5f167bbd2b
nilanmeegoda99 <nilanmeegoda@gmail.com>
2020-09-28 23:10:27 +0530

    Added recycler view to fetch data from firestore

--- totestrecyclerview/app/src/androidTest/java/com/example/totestrecyclerview/ExampleInstrumentedTest.java
+++ totestrecyclerview/app/src/androidTest/java/com/example/totestrecyclerview/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.activityone;
+package com.example.totestrecyclerview;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.activityone", appContext.getPackageName());
+        assertEquals("com.example.totestrecyclerview", appContext.getPackageName());
     }
 }
